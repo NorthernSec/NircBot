a287d88ed12464a0d94c10ba98423ece0f311d78
PidgeyL <pieterjan.moreels@gmail.com>
2017-11-08 21:39:15 +0100

    update plugins plugin

--- plugins/Plugins.py
+++ plugins/Plugins.py
@@ -1,6 +1,8 @@
+import os
+
 class Plugins:
     name     = "Plugins"
-    commands = ['list']
+    commands = ['list', 'load', 'unload']
 
     def __init__(self, **args):
         pass
@@ -10,7 +12,7 @@ class Plugins:
                       channel=None, ident=None, host=None, **args):
         if not (bot and channel and command):
             return
-        if command == 'list':
+        if   command == 'list':
             commands = bot.plugManager.hooks['commands']
             commands = [(c, commands[c].name) for c in sorted(commands.keys())]
             commands = [('Command', 'Plugin'), ('-----', '-----')] + commands
@@ -18,3 +20,24 @@ class Plugins:
             for c in commands:
                 command = c[0]+' '*(maxlen-len(c[0]))
                 bot.say(channel, "%s - %s"%(command, c[1]))
+
+        elif command == 'load':
+            try:
+                name, settings = arguments.split(' ', 1)
+                mode = 'load'
+            except:
+                name, mode, settings = arguments, 'default', None
+            path = 'plugins/%s'%os.path.basename(name)
+            try:
+                bot.plugManager.load_plugin(path, mode, settings)
+                bot.say(channel, 'Plugin loaded')
+            except:
+                bot.say(channel, 'Could not load plugin')
+
+        elif command == 'unload':
+            try:
+                bot.plugManager.unload_plugin(arguments)
+                bot.say(channel, "Plugin unloaded")
+            except Exception as e:
+                print(e)
+                bot.say(channel, "Could not unload plugin")
