939beac3cb528e8998950fdd138a8d16d7fd5717
Pidgey <pidgey@localhost>
2017-11-12 16:49:23 -0500

    Text operations plugin

---
+++ plugins/TextOperations.py
@@ -0,0 +1,14 @@
+class TextOperations:
+    name     = "TextOperations"
+    commands = ['say', 'reverse', 'upper', 'lower']
+    def __init__(self, **args):
+        pass
+
+    def command(self, bot=None, command=None, arguments=None, nick=None,
+                      channel=None, ident=None, host=None, **args):
+        if not (bot and arguments):
+            return
+        if   command == 'say':     return arguments
+        elif command == 'reverse': return arguments[::-1]
+        elif command == 'upper':   return arguments.upper()
+        elif command == 'lower':   return arguments.lower()
